50b7042fa24452c00725895b98ccdd3b0e56bd72
Jamey Kirk <jameykirk@gmail.com>
2021-05-29 14:11:10 +0900

    file split done

--- Assignment09_JKirk.py
+++ Assignment09_JKirk.py
@@ -1,15 +1,57 @@
 """ 
 Description: 
- - Concatenates three PDF files together as "MergedFile.pdf".
+ - Splits 1 PDF file, inserts a file in the middle, and merges everything into one file.
+
 Author: Jamey Kirk
-Date: 05.28.2021
+Date: 05.29.2021
 Assignment: 08
 Class: CIS 112
 """
 
-import pathlib
-from PyPDF2 import PdfFileMerger
+import pathlib, os
+from PyPDF2 import PdfFileMerger, PdfFileReader, PdfFileWriter
 from pathlib import Path
+
+""" global variables """
+mainPDf = "pythonlearn_ch8MissingPages.pdf"
+insertPDF = "From_pythonlearn_ch8.pdf"
+section1 = PdfFileWriter()
+section2 = PdfFileWriter()
+
+""" functions """
+
+""" 
+`for in` loop gets pages from input file and adds them to output file
+"""
+def getPages(n1, n2, section):
+    for pageN in range (n1, n2):
+        page = input_pdf.getPage(pageN)
+        section.addPage(page)
+
+""" 
+after pages have been added to file, a new file is created with the user's file name
+"""
+def outputPDF(section):
+    with Path (userFileName).open(mode="wb") as outputFile:
+        section.write(outputFile)
+
+
+""" main program """
+# create section 1 (beginning section)
+userFileName = "section1.pdf"
+main_pdf_path = os.path.join(pathlib.Path.cwd(), mainPDf)
+input_pdf = PdfFileReader(main_pdf_path)
+
+getPages(0, 4, section1)
+outputPDF(section1)
+
+# create section 2 (end section)
+userFileName = "section2.pdf"
+
+getPages(4, 12, section2)
+outputPDF(section2)
+
+
 
 
 """ Assignment 08
